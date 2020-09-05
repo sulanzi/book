7505cf313cfd0242be45c20c2afe86529e1e8681
sulanzi2 <1932059001@qq.com>
2020-09-05 19:19:00 +0800

    book2

---
+++ src/cn/suhl/book2.java
@@ -0,0 +1,13 @@
+package cn.suhl;
+
+public class book2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("book2");
+	}
+
+}
